a4f4c16f97c8b34bc956c41205b70581a62b09b3
Jeff Douglas <jeff@jeffdouglas.com>
2014-07-25 09:08:08 -0400

    comments

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
   "labix.org/v2/mgo/bson"
 )
 
+// the Crib struct that we can serialize and deserialize into Mongodb
 type Crib struct {
   Handle string `form:"handle"`
   URL  string `form:"url"`
@@ -16,8 +17,11 @@ type Crib struct {
   Description string `form:"description"`
 }
 
-/* Multi-
-   called we initialize a Mongo session on localhost. DB() returns a martini.Handler which will be called on every request. We simply clone the session for every request and make sure it is closed once the request is done being processed. The important bit is the call to c.Map. This maps an instance of *mgo.Database to our request context. This allows all subsequent handler functions to specify a *mgo.Database as an argument and get it injected.
+/* 
+   the function returns a martini.Handler which is called on each request. We simply clone 
+   the session for each request and close it when the request is complete. The call to c.Map 
+   maps an instance of *mgo.Database to the request context. Then *mgo.Database
+   is injected into each handler function.
 */
 func DB() martini.Handler {
   session, err := mgo.Dial(os.Getenv("MONGO_URL")) // mongodb://localhost
@@ -27,18 +31,20 @@ func DB() martini.Handler {
 
   return func(c martini.Context) {
     s := session.Clone()
-    c.Map(s.DB(os.Getenv("MONGO_DB")))
+    c.Map(s.DB(os.Getenv("MONGO_DB"))) // local
     defer s.Close()
     c.Next()
   }
 }
 
+// function to return an array of all Cribs from mondodb
 func All(db *mgo.Database) []Crib {
   var cribs []Crib
   db.C("cribs").Find(nil).All(&cribs)
   return cribs
 }
 
+// function to return a specific Crib by handle
 func Fetch(db *mgo.Database, handle string) Crib {
   var crib Crib
   db.C("cribs").Find(bson.M{"handle": handle}).One(&crib)
@@ -48,20 +54,30 @@ func Fetch(db *mgo.Database, handle string) Crib {
 func main() {
 
   m := martini.Classic()
+  // specify the layout to use when rendering HTML
   m.Use(render.Renderer(render.Options {
     Layout: "layout",
   }))
+  // use the Mongo middleware
   m.Use(DB())
 
+  // list of all cribs
   m.Get("/", func(r render.Render, db *mgo.Database) {
     r.HTML(200, "list", All(db))
   })  
 
+  /* 
+    create a new crib the form submission. Contains some martini magic. The call 
+    to binding.Form(Crib{}) parses out form data when the request comes in. 
+    It binds the data to the struct, maps it to the request context  and
+    injects into our next handler function to insert into Mongodb.
+ */   
   m.Post("/", binding.Form(Crib{}), func(crib Crib, r render.Render, db *mgo.Database) {
     db.C("cribs").Insert(crib)
     r.HTML(200, "list", All(db))
   })  
 
+  // display the crib for a specific user
   m.Get("/:handle", func(params martini.Params, r render.Render, db *mgo.Database) {
     r.HTML(200, "display", Fetch(db, params["handle"]))    
   })    
